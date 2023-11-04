fd941978874c2d302b11d0d6f2d1b1de92b94f9c
MoizArif78987 <moizarif100@gmail.com>
2023-11-04 16:05:47 +0500

    Create form updated with form title and category of form updated

--- src/component/addform.jsx
+++ src/component/addform.jsx
@@ -30,31 +30,133 @@ export default function Addform() {
     setTitlecount(updatedTitlecount);
   };
 
+  const [selectedOption, setSelectedOption] = useState("Student");
+  const handleRadioChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   return (
     <>
       <div className="AddformPage">
         <div className="formcontainer">
           <form>
+            
+            <div className="FormSpecs">
+              <div className="titleInput">
+                <center>
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    style={{
+                      height: "60px",
+                      width: "50%",
+                      background: "transparent",
+                      color: "white",
+                      border: "1px solid white",
+                      borderRadius: "20px",
+                      fontSize: "35px",
+                      fontWeight: "800",
+                      letterSpacing: "5px",
+                      marginTop: "10px",
+                      textAlign: "center",
+                    }}
+                    placeholder="Enter the title for form"
+                  />
+                </center>
+              </div>
+
+              <div className="category">
+                <label>
+                  <input
+                    type="radio"
+                    value="Student"
+                    checked={selectedOption === "Student"}
+                    onChange={handleRadioChange}
+                  />
+                  Student
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    value="Final Year Student"
+                    checked={selectedOption === "Final Year Student"}
+                    onChange={handleRadioChange}
+                  />
+                  Final Year Student
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    value="Alumni"
+                    checked={selectedOption === "Alumni"}
+                    onChange={handleRadioChange}
+                  />
+                  Alumni
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    value="Organization"
+                    checked={selectedOption === "Organization"}
+                    onChange={handleRadioChange}
+                  />
+                  Organization
+                </label>
+              </div>
+            </div>
+
+
             {titlecount.map((index, titleIndex) => (
               <div key={titleIndex}>
                 <div className="titleContainer">
-                  <input className="TitleInput" type="text" placeholder="Enter Title" />
+                  <input
+                    className="TitleInput"
+                    type="text"
+                    placeholder="Enter Title"
+                  />
                   {titleIndex > 0 ? (
-                    <button className="removebtn" onClick={(e) => removeTitle(titleIndex, e)}>-</button>
-                  ):<button className="add-btn" onClick={(e) => addTitle(e)}>+</button>}
+                    <button
+                      className="removebtn"
+                      onClick={(e) => removeTitle(titleIndex, e)}
+                    >
+                      -
+                    </button>
+                  ) : (
+                    <button className="add-btn" onClick={(e) => addTitle(e)}>
+                      +
+                    </button>
+                  )}
                 </div>
                 {index.map((i, k) => (
                   <div className="questionContainer" key={k}>
-                    <input className="QuestionInput" type="text" placeholder="Enter Question" />
+                    <input
+                      className="QuestionInput"
+                      type="text"
+                      placeholder="Enter Question"
+                    />
                     {k === 0 ? (
-                      <button className="addbtn" onClick={(e) => addQuestion(titleIndex, e)}>+</button>
+                      <button
+                        className="addbtn"
+                        onClick={(e) => addQuestion(titleIndex, e)}
+                      >
+                        +
+                      </button>
                     ) : (
-                      <button className="removebtn" onClick={(e) => removeQuestion(titleIndex, k, e)}>-</button>
+                      <button
+                        className="removebtn"
+                        onClick={(e) => removeQuestion(titleIndex, k, e)}
+                      >
+                        -
+                      </button>
                     )}
                   </div>
                 ))}
               </div>
             ))}
+
+            <button className="submit-btn">Submit</button>
+
           </form>
         </div>
       </div>
